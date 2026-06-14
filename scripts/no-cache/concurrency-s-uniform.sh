8d764990cf0821277742412ad0c7c1f80ca28518
minhdangnguyenn <dangminh214@gmail.com>
2026-06-14 11:10:52 +0200

    test plot

--- scripts/no-cache/concurrency-s-uniform.sh
+++ scripts/no-cache/concurrency-s-uniform.sh
@@ -1,1 +1,2 @@
-cargo run --release --bin workload -- --experiment concurrency-s-uniform -c 24 read --rows 1000
+ID=${1:-1}
+cargo run --release --bin workload -- --experiment concurrency-s-uniform --id "$ID" -c 24 read --rows 1000
