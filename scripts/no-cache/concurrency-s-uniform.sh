c190b57b53d27a4992a4fc4e42d110775b564ee7
minhdangnguyenn <dangminh214@gmail.com>
2026-06-16 20:08:32 +0200

    try concurrency

--- scripts/no-cache/concurrency-s-uniform.sh
+++ scripts/no-cache/concurrency-s-uniform.sh
@@ -1,2 +1,2 @@
 ID=${1:-1}
-cargo run --release --bin workload -- --experiment concurrency-s-uniform --id "$ID" -c 24 read --rows 1000
+cargo run --release --bin workload -- --experiment no-cache-concurrency-s-uniform --id "$ID" -c 24 read --rows 1000
