e17849b26712328bb9a2b46f88e9bcfae393e0f7
minhdangnguyenn <dangminh214@gmail.com>
2026-06-17 11:26:46 +0200

    restructure

---
+++ scripts/cloud/no-cache/uniform-s.sh
@@ -0,0 +1,5 @@
+ID=${1:-1}
+HOST_FLAG=""
+
+[ -n "${TOYDB_HOSTS:-}" ] && HOST_FLAG="-H $TOYDB_HOSTS"
+cargo run --release --bin workload -- $HOST_FLAG --experiment cloud-no-cache-s-uniform --id "$ID" read --rows 1000
