826734d67c5c8476ba44f1873f6b1363cddecfed
minhdangnguyenn <dangminh214@gmail.com>
2026-06-23 12:00:22 +0200

    save

---
+++ scripts/cloud/exp3/1000/fifo/c4/l/zipf.sh
@@ -0,0 +1,4 @@
+ID=${1:-1}
+HOST_FLAG=""
+[ -n "${TOYDB_HOSTS:-}" ] && HOST_FLAG="-H $TOYDB_HOSTS"
+cargo run --release --bin workload -- $HOST_FLAG --experiment cloud-exp3-cache1000-fifo-c4-l-zipf --id "$ID" -c 4 read --rows 10000 --cache --cache-size 1000 --dist zipf --fifo
