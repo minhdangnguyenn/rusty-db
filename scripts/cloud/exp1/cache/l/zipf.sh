f32a94d7c5c6950a5be33dec10ba1f71b99e7875
minhdangnguyenn <dangminh214@gmail.com>
2026-07-01 23:03:03 +0200

    delete old charts

--- scripts/cloud/exp1/cache/l/zipf.sh
+++ scripts/cloud/exp1/cache/l/zipf.sh
@@ -1,4 +1,5 @@
 ID=${1:-1}
 HOST_FLAG=""
 [ -n "${TOYDB_HOSTS:-}" ] && HOST_FLAG="-H $TOYDB_HOSTS"
-cargo run --release --bin workload -- $HOST_FLAG --experiment cloud-exp1-cache-l-zipf --id "$ID" read --rows 10000 --cache --dist zipf
+cargo run --release --bin workload -- $HOST_FLAG --duration 30 --experiment exp1-cache-l-zipf \
+--id "$ID" read --rows 10000 --cache --dist zipf
--- scripts/cloud/exp1/no-cache/s/zipf.sh
+++ scripts/cloud/exp1/no-cache/s/zipf.sh
@@ -1,4 +1,5 @@
 ID=${1:-1}
 HOST_FLAG=""
 [ -n "${TOYDB_HOSTS:-}" ] && HOST_FLAG="-H $TOYDB_HOSTS"
-cargo run --release --bin workload -- $HOST_FLAG --experiment cloud-exp1-no-cache-s-zipf --id "$ID" read --rows 1000  --dist zipf
+cargo run --release --bin workload -- $HOST_FLAG --duration 30 --experiment exp1-no-cache-s-zipf \
+--id "$ID" read --rows 1000  --dist zipf
