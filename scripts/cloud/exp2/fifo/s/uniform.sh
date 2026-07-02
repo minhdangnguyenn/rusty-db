5f95482baeccd84352abe6ea42da68295dd9b529
minhdangnguyenn <dangminh214@gmail.com>
2026-07-02 16:10:08 +0200

    save experiment 2 s fifo

--- scripts/cloud/exp2/fifo/s/uniform.sh
+++ scripts/cloud/exp2/fifo/s/uniform.sh
@@ -1,4 +1,4 @@
 ID=${1:-1}
 HOST_FLAG=""
 [ -n "${TOYDB_HOSTS:-}" ] && HOST_FLAG="-H $TOYDB_HOSTS"
-cargo run --release --bin workload -- $HOST_FLAG --experiment cloud-exp2-cache5000-fifo-s-uniform --id "$ID" read --rows 1000 --cache --cache-size 5000 --fifo
+cargo run --release --bin workload -- $HOST_FLAG --experiment exp2-cache5000-fifo-s-uniform --id "$ID" read --rows 1000 --cache --cache-size 5000 --fifo
