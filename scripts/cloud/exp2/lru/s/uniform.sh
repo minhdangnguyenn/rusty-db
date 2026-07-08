8b67dc605c5faf863ba338107586fb309775757d
Nguyen, Dang Minh <dang_minh.nguyen@stud.tu-darmstadt.de>
2026-07-08 21:20:55 +0200

    update scripts

--- scripts/cloud/exp2/lru/s/uniform.sh
+++ scripts/cloud/exp2/lru/s/uniform.sh
@@ -1,4 +1,5 @@
 ID=${1:-1}
 HOST_FLAG=""
 [ -n "${TOYDB_HOSTS:-}" ] && HOST_FLAG="-H $TOYDB_HOSTS"
-cargo run --release --bin workload -- $HOST_FLAG --experiment cloud-exp2-cache5000-lru-s-uniform --id "$ID" read --rows 1000 --cache --cache-size 5000
+cargo run --release --bin workload -- $HOST_FLAG --experiment exp2-lru-s-uniform \
+--id "$ID" read --rows 1000 --cache --cache-size 5000
--- scripts/cloud/exp2/lru/s/zipf.sh
+++ scripts/cloud/exp2/lru/s/zipf.sh
@@ -1,4 +1,5 @@
 ID=${1:-1}
 HOST_FLAG=""
 [ -n "${TOYDB_HOSTS:-}" ] && HOST_FLAG="-H $TOYDB_HOSTS"
-cargo run --release --bin workload -- $HOST_FLAG --experiment cloud-exp2-cache5000-lru-s-zipf --id "$ID" read --rows 1000 --cache --cache-size 5000  --dist zipf
+cargo run --release --bin workload -- $HOST_FLAG --experiment exp2-lru-s-zipf \
+--id "$ID" read --rows 1000 --cache --cache-size 5000  --dist zipf
