9fb75d8f38a5f2de3e7bcf7435fe02e6fc62fcc3
Minh Nguyen <dangminh214@gmail.com>
2026-08-15 12:36:20 +0200

    sorted import

--- plot/exp1/compare-throughput.py
+++ plot/exp1/compare-throughput.py
@@ -8,9 +8,9 @@ sys.path.insert(0, os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 from plot.config import (
     FIGSIZE,
-    compute_ci_from_dir,
-    NAVY,
     LIGHT_RED,
+    NAVY,
+    compute_ci_from_dir,
     grid_style,
     legend_pos,
 )
